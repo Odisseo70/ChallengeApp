f2d73ab0d9029421611eb39c5055253c10c6cdfc
Odisseo 70 <s.folino70@gmail.com>
2023-11-18 20:26:47 +0100

    Zadania dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+var age = 33;
+bool isWoman = true ;
+var name = " Ewa " ;
+
+if ( isWoman  == true && age < 30 )
+{
+    Console.WriteLine ("Kobieta poniżej 30 lat ");
+}
+else if (name == " Ewa " && age == 33)
+{
+    Console.WriteLine("Ewa ,lat 33 ");
+}
+else if ( isWoman != true && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyżna ");
+}
